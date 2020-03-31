5d2df964973069e754bf453c884476c25ed292d0
Neihy Rincon <neihy.rincon@gmail.com>
2020-03-31 17:59:57 +0000

    finalizing task of tail

--- tail.js
+++ tail.js
@@ -8,8 +8,12 @@ const assertEqual = function(actual, expected) {
 };
 
 // FUNCTION TAIL
-const tail = function(value){
+const tail = function(value) {
+  let newValue = (value.slice(1));
+  return newValue;
+};
 
-}
-
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3);
 
