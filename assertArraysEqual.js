429aa6f04c6444b001ccba612298d55e6e1188f3
Neihy Rincon <neihy.rincon@gmail.com>
2020-04-01 02:24:49 +0000

    adding an appropiate message to the assert array exercise

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+// Get the value type
+const eqArrays = function (actual, expected) {
+  // Compare the length of the two arrays
+  if (actual.length === expected.length) {
+    for (i=0; i < actual.length; i++) {
+      // Compare type and value for the two items 
+      if (typeof(actual[i]) !== typeof(expected[i]) || actual[i] !== expected[i]){
+        return false
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+console.log((eqArrays([1, 2, 3], [1, 3, 3])) ? `Assertion Passed`:`Assertion Failed`);
