853689360874333ef6640195aaedf03ebc45ffbc
Neihy Rincon <neihy.rincon@gmail.com>
2020-04-01 18:57:46 +0000

    middle exercise done

--- middle.js
+++ middle.js
@@ -1,31 +1,41 @@
 // TEST/ASSERTION FUNCTIONS
-const eqArrays = function (actual, expected) {
+const eqArrays = function(arrayA, arrayB) {
   // Compare the length of the two arrays
-  if (actual.length === expected.length) {
-    for (i=0; i < actual.length; i++) {
-      // Compare type and value for the two items 
-      if (typeof(actual[i]) !== typeof(expected[i]) || actual[i] !== expected[i]){
-        return false
+  if (arrayA.length === arrayB.length) {
+    for (let itemIndex in arrayA) {
+      let valueActual = arrayA[itemIndex];
+      let valueExp = arrayB[(arrayB.indexOf(arrayA[itemIndex]))];
+      if (typeof(valueActual) !== typeof(valueExp) || valueActual !== valueExp) {
+        return false;
       }
     }
     return true;
   } else {
     return false;
   }
-}
+};
 
 const assertArraysEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
+  return (eqArrays(actual, expected) ? `Assertion Passed` : `Assertion Failed`);
 };
 
 // ACTUAL FUNCTION
 const middle = function(array) {
-  //...
-}
+  let index = array[Math.floor(array.length / 2)];
+  let midIndex = [];
+  if (array.length <= 2) {
+    return midIndex;
+  } else if (array.length % 2 === 0) {
+    midIndex.push(array[index - 2]);
+    midIndex.push(array[index - 1]);
+  } else {
+    midIndex.push(array[index - 1 ]);
+  }
+  return midIndex;
+};
 
 // TEST CODE
-// ...
+console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [ 3 ]));
+console.log(assertArraysEqual(middle([1, 2 ]), [  ]));
+console.log(assertArraysEqual(middle([1 ]), [  ]));
+console.log(assertArraysEqual(middle([1, 2, 3, 4 , 5, 6 ]), [ 3, 4]));
