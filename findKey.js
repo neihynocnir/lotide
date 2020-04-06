ea0e048eaadacaec29eae498e27c45e3944c86fa
Neihy Rincon <neihy.rincon@gmail.com>
2020-04-06 23:06:17 +0000

    refactoring findKey

--- findKey.js
+++ findKey.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const findKey = function(info, callback) {
   let infoKeys = Object.keys(info);
   for (let key of infoKeys) {
@@ -15,16 +7,5 @@ const findKey = function(info, callback) {
   }
 };
 
-const hotels = {
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-};
+module.exports = findKey;
 
-
-const findHotel = findKey(hotels, x => x.stars === 2);
-console.log(findHotel);
-assertEqual(findHotel,'noma');
---
+++ test/findKeyTest.js
@@ -0,0 +1,20 @@
+const assert = require('chai').assert;
+const findKey = require('../findKey');
+
+describe("#findKey", () => {
+  it("returns undefined when doesn't exist in the input", () => {
+    const input = {'Blue Hill':{ stars: 1 },'Akaleri':{ stars: 3 },'noma':{ stars: 2 }};
+    const callback = x => x.stars === 4;
+    const output = undefined;
+    assert.strictEqual(findKey(input, callback),output);
+  });
+
+
+  it("returns 'noma' for {'Blue Hill':{ stars: 1 },'Akaleri':{ stars: 3 },'noma':{ stars: 2 }}' with callback  x => x.stars === 2", () => {
+    const input = {'Blue Hill':{ stars: 1 },'Akaleri':{ stars: 3 },'noma':{ stars: 2 }};
+    const callback = x => x.stars === 2;
+    const output = 'noma';
+    assert.strictEqual(findKey(input, callback),output);
+  });
+
+});
