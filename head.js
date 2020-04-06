5b680029179798f92ba2cc7e3cd2ec5a112017f1
Neihy Rincon <neihy.rincon@gmail.com>
2020-04-06 17:21:47 +0000

    lotide refactor example

--- head.js
+++ head.js
@@ -1,13 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// FUNCTION HEAD
 const head = function(items) {
   let firstOne;
   if (items === undefined) {
@@ -20,5 +10,4 @@ const head = function(items) {
   return firstOne;
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
