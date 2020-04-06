c27f4b863016447f781e5e27d07402df15f6374f
Neihy Rincon <neihy.rincon@gmail.com>
2020-04-06 17:27:54 +0000

    refactoring tail

--- tail.js
+++ tail.js
@@ -1,19 +1,6 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// FUNCTION TAIL
 const tail = function(value) {
   let newValue = (value.slice(1));
   return newValue;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
-
+module.exports = tail;
