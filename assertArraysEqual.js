3bf88d7018bb5ce5bc7f43ed6cfa5dab2ea37bd9
Neihy Rincon <neihy.rincon@gmail.com>
2020-04-06 17:46:49 +0000

    refactoring assertArrayEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,7 @@
-// FUNCTION IMPLEMENTATION
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(actual, expected) {
-  return (eqArrays(actual, expected) ? `Assertion Passed` : `Assertion Failed`);
+  console.log((eqArrays(actual, expected) ? `Assertion Passed` : `Assertion Failed`));
 };
 
-const eqArrays = function(arrayA, arrayB) {
-  // Compare the length of the two arrays
-  if (arrayA.length === arrayB.length) {
-    for (let itemIndex in arrayA) {
-      let valueActual = arrayA[itemIndex];
-      let valueExp = arrayB[(arrayB.indexOf(arrayA[itemIndex]))];
-      if (typeof(valueActual) !== typeof(valueExp) || valueActual !== valueExp) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-};
-
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+module.exports = assertArraysEqual;
