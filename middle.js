c9b16eb9b21e6e02f211ed89a315ab103268668d
Neihy Rincon <neihy.rincon@gmail.com>
2020-04-06 17:51:36 +0000

    refactoring middle

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-// TEST/ASSERTION FUNCTIONS
-const eqArrays = function(arrayA, arrayB) {
-  // Compare the length of the two arrays
-  if (arrayA.length === arrayB.length) {
-    for (let itemIndex in arrayA) {
-      let valueActual = arrayA[itemIndex];
-      let valueExp = arrayB[(arrayB.indexOf(arrayA[itemIndex]))];
-      if (typeof(valueActual) !== typeof(valueExp) || valueActual !== valueExp) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const assertArraysEqual = function(actual, expected) {
-  return (eqArrays(actual, expected) ? `Assertion Passed` : `Assertion Failed`);
-};
-
-// ACTUAL FUNCTION
 const middle = function(array) {
   let index = array[Math.floor(array.length / 2)];
   let midIndex = [];
@@ -34,8 +12,4 @@ const middle = function(array) {
   return midIndex;
 };
 
-// TEST CODE
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [ 3 ]));
-console.log(assertArraysEqual(middle([1, 2 ]), [  ]));
-console.log(assertArraysEqual(middle([1 ]), [  ]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4 , 5, 6 ]), [ 3, 4]));
+module.exports = middle;
