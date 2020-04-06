c09949f7af2c122272d84e4e51171378f5f257fc
Neihy Rincon <neihy.rincon@gmail.com>
2020-04-06 23:42:16 +0000

    updating index.js

--- index.js
+++ index.js
@@ -12,16 +12,15 @@ const takeUntil       = require('./takeUntil');
 const without         = require('./without');
 
 module.exports = {
-  countLetters: countLetters,
-  countOnly: countOnly,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  head:   head,
-  letterPositions: letterPositions,
-  map: map,
-  middle: middle,
-  tail:   tail,
-  takeUntil: takeUntil,
-  without: without
-
+  countLetters,
+  countOnly,
+  findKey,
+  findKeyByValue,
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without
 };
